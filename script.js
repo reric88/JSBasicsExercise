83cca807de7d1e22771e4a42aed5ee054ce0a5f3
Eric Collard <ruffyreborn@gmail.com>
2023-03-14 23:15:49 -0500

    Completed assignment for Commonwealth Coders

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// QUESTION 1
+console.log(27);
+
+// QUESTION 2
+// Numbers, Strings, Booleans, Null and Undefined
+
+// QUESTION 3
+// +. -, *, /, **, %
+
+// QUESTION 4
+console.log(1%5);
+console.log(5%4);
+
+// QUESTION 5
+console.log('"I\'M MARY POPPINS, YA\'LL!"');
+
+// QUESTION 6
+console.log("Hawk" + "eye");
+
+// QUESTION 7
+console.log("Eric".length + "Collard".length);
+
+// QUESTION 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS 1
+console.log("\\");
+
+// BONUS 2
+console.log(5**4-9 + "            ___");
+
+// BONUS 3
+console.log("I".length % "Am".length - "Iron".length % "Man".length + "          ,-'..-..'-,");
+
+// BONUS 4
+console.log("CAP"[1] + "        .'.##*,-,*##.'.");
+console.log("VISION"[0] + "       / /##/ /#\\ \\##\\ \\   _     _   _____    _    _     ___      _____    _____      ___");
+console.log("OKOYE"[4] + "      / /##/ /###\\ \\##\\ \\ | |   | | |  ___|  | \\  | |  .'   '.   |  ___|  | :''-`: ,-'   '-,");
+console.log("NICK FURY"[0] + "     : |##/ /#####\\ \\##| :| |   | | | |__    | \\\\ | | | .'''._|  | |__    | |_-' ; | ;'''\\ :");
+console.log("GROOT"[0] + "     : |##| ``````` |##| :\\ \\   / / |  __|   | |\\\\| | | |  ___   |  __|   | .. -'  | -...,''");
+console.log("DOCTOR STRANGE"[13] +"      \\ \\#| |#####| |#/ /  \\ \\ / /  | |      | | \\  | | | |__ |  | |      | | \\ \\   `'--., `-");
+console.log("THOR"[3] + "       \\ \\| |#####| |/ /    \\ v /   | |____  | |  | | |  -.,: |  | |____  | |  | | |'\\.,./  /");
+console.log("STAR-LORD"[0] + "        '-.__#####__.-'      '.'    |______| |_|  |_|  '-.__.-'  |______| |_|  |_|  '-.__.-'");
+
+// BONUS 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
